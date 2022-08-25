ff98fe0b05e090aeaa4137ef369534d7c28ec73d
vvede <v.vedeshenkov@gmail.com>
2022-08-24 23:47:52 -0700

    User info added

--- src/day2/RegisterPage.java
+++ src/day2/RegisterPage.java
@@ -3,5 +3,6 @@ package day2;
 public class RegisterPage {
     public static void main(String[] args) {
         System.out.println("Page was created");
+        System.out.println("User information added");
     }
 }
