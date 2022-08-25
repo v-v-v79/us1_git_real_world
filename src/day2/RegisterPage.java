548f3f1fc2ac7df16c98dc7d9cf57c62ef17e578
vvede <v.vedeshenkov@gmail.com>
2022-08-24 23:50:38 -0700

    Finish button added

--- src/day2/RegisterPage.java
+++ src/day2/RegisterPage.java
@@ -4,5 +4,6 @@ public class RegisterPage {
     public static void main(String[] args) {
         System.out.println("Page was created");
         System.out.println("User information added");
+        System.out.println("Finish button added");
     }
 }
