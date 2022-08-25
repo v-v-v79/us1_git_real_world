c96cb9724d0cce3eed3cac8b57fb37f49fc0fb1b
vvede <v.vedeshenkov@gmail.com>
2022-08-25 00:17:19 -0700

    edited

--- src/day2/RegisterPage.java
+++ src/day2/RegisterPage.java
@@ -4,6 +4,5 @@ public class RegisterPage {
     public static void main(String[] args) {
         System.out.println("Page was created");
         System.out.println("User information added");
-        System.out.println("Finish button added");
     }
 }
