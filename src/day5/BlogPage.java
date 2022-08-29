1dcbd0c172481c3053363956eba3e108941dcfd1
vvede <v.vedeshenkov@gmail.com>
2022-08-28 20:19:01 -0700

    Blog Page News Block was added

--- src/day5/BlogPage.java
+++ src/day5/BlogPage.java
@@ -3,5 +3,6 @@ package day5;
 public class BlogPage {
     public static void main(String[] args) {
         System.out.println("Blog Page was created");
+        System.out.println("News Block was added");
     }
 }
