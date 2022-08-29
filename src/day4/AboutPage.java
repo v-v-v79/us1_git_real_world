d1c93a6da0304a53783b995cfb3582a3fa325ba5
vvede <v.vedeshenkov@gmail.com>
2022-08-28 19:35:57 -0700

    About Page text was added

--- src/day4/AboutPage.java
+++ src/day4/AboutPage.java
@@ -3,5 +3,6 @@ package day4;
 public class AboutPage {
     public static void main(String[] args) {
         System.out.println("About page was created");
+        System.out.println("About Page text added");
     }
 }
