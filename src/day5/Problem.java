c1393d11de6714fc73a5e6d5485a34a80d79e4ea
vvede <v.vedeshenkov@gmail.com>
2022-08-28 21:25:17 -0700

    Problem Page was created

---
+++ src/day5/Problem.java
@@ -0,0 +1,10 @@
+package day5;
+
+public class Problem {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+        //some code is here
+    }
+}
